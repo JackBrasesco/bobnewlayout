5a8f8c269921156266191a0aed95ad44845a1ea3
JackBrasesco <jacbras@nuevaschool.org>
2016-09-27 09:10:53 -0700

    Classadoo autocommit

--- bob.js
+++ bob.js
@@ -2,7 +2,7 @@ var gameInput = $("#game-input");
 var gameOutput  = $("#game-output");
 gameInput.keydown(function(keydownEvent) {
   // the key code for enter is 13
-
+console.log(gameOutput)
   if (keydownEvent.keyCode == 13) {  
     parseText(gameInput.val()); 
     gameInput.val();
