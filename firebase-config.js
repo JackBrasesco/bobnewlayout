ad03dd1a6e4e7c814f612acad5778844bc4fd42d
JackBrasesco <jacbras@nuevaschool.org>
2016-10-09 11:46:45 -0700

    Classadoo autocommit

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-vvar firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyB4n3T-wB2G2UnIrMUU3hC7SqoXhPEb4L0",
   authDomain: "classadoo-dev.firebaseapp.com",
   databaseURL: "https://classadoo-dev.firebaseio.com"	
